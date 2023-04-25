646e00d6cd1adcb5c8a7b48fbd314f4e9575bbc9
Michael Stewart <stewarmc@jmu.edu>
2023-04-25 10:51:09 -0400

    -1 step

--- script.js
+++ script.js
@@ -8,10 +8,53 @@ pw.addEventListener('keyup', (ev) => {
     const structuredContent = JSON.parse(autoContent)
     console.log(structuredContent)
     console.log(structuredContent.next)
-    req.open("GET", `${structuredContent.next}${pw.value}.json`);
-    req.send();
+    // req.open("GET", `${structuredContent.next}${pw.value}.json`);
+    // req.send();
+    const firstRequestPromise = fetch(`${structuredContent.next}${pw.value}.json`);
+    firstRequestPromise
+      .then(processRespAsJSON)
+      .then(putResponse1InDOM)
+      .then(make2ndReq)
+      .then(processRespAsJSON)
+      .then(put2ndRespInDOM)
+      // .then(make3rdReq)
+      // .then(process3rdResp)
+      // .then(put3rdRespInDOM)
+      .catch((err) => {
+        console.error(`\n\n\nMY ERROR HANDLER: ${err}`);
+        return err;
+      });
   }
 })
+const processRespAsJSON = (resp) => {
+  console.log('processing response')
+  const data = resp.json();
+  // console.log('data', data) //d-oh! this is still the promise 🤦‍♂️
+  return data;
+  // console.log(JSON.parse(resp.text)
+}
+
+const putResponse1InDOM = (jsonRepOfResponseBodyInMemory) => {
+  console.log("jsonRepOfResponseBodyInMemory", jsonRepOfResponseBodyInMemory);
+  document.getElementById("i1-out").innerText = JSON.stringify(jsonRepOfResponseBodyInMemory);
+  return jsonRepOfResponseBodyInMemory.steps[0].req;
+}
+const make2ndReq = (url) => {
+  return fetch(url)
+}
+
+const put2ndRespInDOM = (data) => {
+  console.log("data", data);
+  document.getElementById("i2-out").innerText = JSON.stringify(
+    data
+  );
+  const numKeys = Object.keys(data).length
+  console.log(numKeys)
+  return numKeys;
+};
+
+
+
 
 let req = new XMLHttpRequest();
 req.addEventListener("load", function (ev) {
@@ -24,31 +67,31 @@ req.addEventListener("load", function (ev) {
   document.getElementById("i1-out").innerText = ev.target.responseText;
 });
 
-let reqWithKeyCount = new XMLHttpRequest();
-reqWithKeyCount.addEventListener("load", function (ev) {
-  console.log("keycount responded");
-  const structuredData = JSON.parse(ev.target.responseText);
-  console.log("structuredData", structuredData);
+// let reqWithKeyCount = new XMLHttpRequest();
+// reqWithKeyCount.addEventListener("load", function (ev) {
+//   console.log("keycount responded");
+//   const structuredData = JSON.parse(ev.target.responseText);
+//   console.log("structuredData", structuredData);
 
-  document.getElementById("i3-out").innerText = ev.target.responseText;
+//   document.getElementById("i3-out").innerText = ev.target.responseText;
 
-  const sum = structuredData.subtotals.reduce((a,b)=>a+b);
-  alert(sum);
-});
+//   const sum = structuredData.subtotals.reduce((a,b)=>a+b);
+//   alert(sum);
+// });
 
-let boredReq = new XMLHttpRequest();
-boredReq.addEventListener("load", function (ev) {
-  console.log("bored responded");
-  const structuredData = JSON.parse(ev.target.responseText);
-  console.log("structuredData", structuredData);
-  const keyCount = Object.keys(structuredData).length;
-  console.log(keyCount);
+// let boredReq = new XMLHttpRequest();
+// boredReq.addEventListener("load", function (ev) {
+//   console.log("bored responded");
+//   const structuredData = JSON.parse(ev.target.responseText);
+//   console.log("structuredData", structuredData);
+//   const keyCount = Object.keys(structuredData).length;
+//   console.log(keyCount);
 
-  document.getElementById("i2-out").innerText = ev.target.responseText;
-  reqWithKeyCount.open(
-    "GET",
-    `https://w3.cs.jmu.edu/stewarmc/scavenge/${keyCount}.json`
-  );
-  reqWithKeyCount.send();
-});
+//   document.getElementById("i2-out").innerText = ev.target.responseText;
+//   reqWithKeyCount.open(
+//     "GET",
+//     `https://w3.cs.jmu.edu/stewarmc/scavenge/${keyCount}.json`
+//   );
+//   reqWithKeyCount.send();
+// });
 
